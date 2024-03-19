4ffac41c4940bcfa8fffbf35ecc502bb98821bc2
Een <ehbowdenianb@gmail.com>
2024-03-18 20:13:39 -0400

    please work

--- Backend/API/database.js
+++ Backend/API/database.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2/promise");
 
 async function connect() {
     const con = await mysql.createConnection({
-        host: "0.0.0.0",
+        host: "localhost",
         port: 3306,
         user: "root",
         password: "password",
