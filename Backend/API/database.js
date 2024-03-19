ca523713a984ac42fc1009abcfec68809aee3ee1
Een <ehbowdenianb@gmail.com>
2024-03-18 20:30:32 -0400

    banana

--- Backend/API/database.js
+++ Backend/API/database.js
@@ -8,7 +8,7 @@ async function connect() {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "",
+        password: "password",
         database: "facebell"
     });
 
